657e98231d2969130cf116d80cf0ddad23f20503
Tracy Wallace <tswwci@hotmail.com>
2020-10-06 15:15:36 -0400

    Init Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AZ_204_Container_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
